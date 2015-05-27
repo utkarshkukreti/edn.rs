e562a53602253be116fa4110f24939a7b5d5b7d9
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-27 20:01:03 +0530

    feat: add `Parser` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 
+pub mod parser;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Value {
     Nil,
---
+++ src/parser.rs
@@ -0,0 +1,4 @@
+pub struct Parser<'a> {
+    str: &'a str,
+    pos: usize
+}

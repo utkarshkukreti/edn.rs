cef5db615449a687be65257ad37efaec2a52ec91
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-27 20:01:31 +0530

    feat(Parser): add `new` function

--- src/parser.rs
+++ src/parser.rs
@@ -2,3 +2,12 @@ pub struct Parser<'a> {
     str: &'a str,
     pos: usize
 }
+
+impl<'a> Parser<'a> {
+    pub fn new(str: &'a str) -> Parser<'a> {
+        Parser {
+            str: str,
+            pos: 0
+        }
+    }
+}

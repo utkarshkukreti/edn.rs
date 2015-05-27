a55c0985c34040ba58a124519f633bc6e2462891
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-05-27 20:00:33 +0530

    feat: add `Value` enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-#[test]
-fn it_works() {
+use std::collections::{BTreeMap, BTreeSet};
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Value {
+    Nil,
+    Boolean(bool),
+    String(String),
+    Char(char),
+    Symbol(String),
+    Keyword(String),
+    Integer(i64),
+    Float(f64),
+    List(Vec<Value>),
+    Vector(Vec<Value>),
+    Map(BTreeMap<Value, Value>),
+    Set(BTreeSet<Value>)
 }

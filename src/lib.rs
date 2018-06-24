af9b9402b65dae34a44a1bce589afe958d038918
Josh Lemer <joshlemer@gmail.com>
2018-06-23 21:41:49 -0400

    Implement From<A> for Value, for various A

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,70 @@ pub enum Value {
     Set(BTreeSet<Value>),
     Tagged(String, Box<Value>),
 }
+
+impl From<bool> for Value {
+    fn from(s: bool) -> Self {
+        Value::Boolean(s)
+    }
+}
+
+impl <'a> From<&'a str> for Value {
+    fn from(s: &'a str) -> Self {
+        Value::String(s.to_string())
+    }
+}
+
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Value::String(s)
+    }
+}
+
+impl From<char> for Value {
+    fn from(s: char) -> Self {
+        Value::Char(s)
+    }
+}
+
+impl From<i64> for Value {
+    fn from(s: i64) -> Self {
+        Value::Integer(s)
+    }
+}
+impl From<f64> for Value {
+    fn from(s: f64) -> Self {
+        Value::Float(OrderedFloat(s))
+    }
+}
+
+impl From<OrderedFloat<f64>> for Value {
+    fn from(s: OrderedFloat<f64>) -> Self {
+        Value::Float(s)
+    }
+}
+
+impl <A> From<Vec<A>> for Value where Value: From<A> {
+    fn from(s: Vec<A>) -> Self {
+        Value::Vector(s.into_iter().map(Value::from).collect())
+    }
+}
+
+impl <K, V>From<BTreeMap<K, V>> for Value
+    where Value: From<K>, Value: From<V>{
+    fn from(s: BTreeMap<K, V>) -> Self {
+        let mut map = BTreeMap::new();
+        for (k, v) in s {
+            map.insert(Value::from(k), Value::from(v));
+        }
+        Value::Map(map)
+    }
+}
+
+impl <A> From<BTreeSet<A>> for Value
+    where Value: From<A>{
+    fn from(s: BTreeSet<A>) -> Self {
+        let mut set = BTreeSet::new();
+        s.into_iter().for_each(|a| {set.insert(Value::from(a));});
+        Value::Set(set)
+    }
+}
